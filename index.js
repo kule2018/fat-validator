832bde5f84d35e8c9acab1ed838850e31d3bcc28
FatGe <duangci@aliyun.com>
2018-11-05 13:49:19 +0800

    fix error global inject => local inject

--- index.js
+++ index.js
@@ -1,92 +1,89 @@
-'use strict'
-
 import eventHandler from './lib/eventHandler'
 import { on, off } from './utils'
 
-export default {
-    install (Vue) {
-        let eventHandler$$1 = null
-        let context = null
-        Vue.directive('validate', {
-            bind (element, binding, vnode) {
-                let { arg: name } = binding
-                const { modifiers, value: rules } = binding
-                const method = Object.keys(modifiers)[0]
+let eventHandler$$1 = null
+let context = null
 
-                context = vnode.context
+export default function (Vue) {
+    Vue.directive('validate', {
+        bind (element, binding, vnode) {
+            let { arg: name } = binding
+            const { modifiers, value: rules } = binding
+            const method = Object.keys(modifiers)[0]
 
-                if (eventHandler$$1) {
-                    eventHandler$$1.bind(context)
-                } else {
-                    eventHandler$$1 = new eventHandler(context)
-                }
+            context = vnode.context
 
-                const handler = function () {
-                    eventHandler$$1.broadcast(name)
-                }
-
-                eventHandler$$1.subscribe({
-                    name,
-                    method,
-                    rules,
-                    element,
-                    handler
-                })
-
-                method && on(element, method, handler)
-            },
-
-            unbind (element, binding) {
-                const { arg: name, modifiers } = binding
-                const method = Object.keys(modifiers)[0]
-                const handler = eventHandler$$1.removeSubscribe(name)
-
-                method && off(element, method, handler)
+            if (eventHandler$$1) {
+                eventHandler$$1.bind(context)
+            } else {
+                eventHandler$$1 = new eventHandler(context)
             }
-        })
 
-        Vue.mixin({
-            data () {
-                return {
-                    validateErrs: {
-                        get (param) {
-                            return this[param]
-                                ? this[param]
-                                : {
-                                    warn: '',
-                                    success: true
-                                }
-                        },
-                        validate (name) {
-                            try {
-                                return eventHandler$$1.broadcast(name)
-                            } catch (error) {
-                                console.warn('Please confirm v-validate is bound.', error)
-                            }
-                        },
-                        validateAll () {
-                            return eventHandler$$1
-                                ? eventHandler$$1.broadcastAll()
-                                : !eventHandler$$1
-                        },
-                        reset (name) {
-                            delete this[name]
-                            context && context.$forceUpdate()
-                        },
-                        resetAll () {
-                            Object.keys(this).forEach(key => {
-                                if (
-                                    this[key] &&
-                                    typeof this[key] !== 'function'
-                                ) {
-                                    delete this[key]
-                                }
-                            })
-                            context && context.$forceUpdate()
+            const handler = function () {
+                eventHandler$$1.broadcast(name)
+            }
+
+            eventHandler$$1.subscribe({
+                name,
+                method,
+                rules,
+                element,
+                handler
+            })
+
+            method && on(element, method, handler)
+        },
+
+        unbind (element, binding) {
+            const { arg: name, modifiers } = binding
+            const method = Object.keys(modifiers)[0]
+            const handler = eventHandler$$1.removeSubscribe(name)
+
+            method && off(element, method, handler)
+        }
+    })
+}
+
+export const validateResult = {
+    data () {
+        return {
+            errors: {
+                get (param) {
+                    return this[param]
+                        ? this[param]
+                        : {
+                            warn: '',
+                            success: true
                         }
+                },
+                validate (name) {
+                    try {
+                        return eventHandler$$1.broadcast(name)
+                    } catch (error) {
+                        console.warn('Please confirm v-validate is bound.', error)
                     }
+                },
+                validateAll () {
+                    return eventHandler$$1
+                        ? eventHandler$$1.broadcastAll()
+                        : !eventHandler$$1
+                },
+                reset (name) {
+                    delete this[name]
+                    context && context.$forceUpdate()
+                },
+                resetAll () {
+                    Object.keys(this).forEach(key => {
+                        if (
+                            this[key] &&
+                            typeof this[key] !== 'function'
+                        ) {
+                            delete this[key]
+                        }
+                    })
+                    context && context.$forceUpdate()
                 }
             }
-        })
-    }
+        }
+    }   
 }
